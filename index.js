4bc06509361b3c2f8a75e64dc31ecba85a766d2e
sabbirjiinat <sabbirshnt@gmail.com>
2023-06-08 09:35:58 +0600

    implement jwt and get all user

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
 const port = process.env.PORT || 5000;
 
 
 //middleware
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
+
+const verifyJWT = (req,res,next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error:true,message:'UnAuthorization access'})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({error:true,message:'UnAuthorization access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 
 
@@ -32,14 +50,21 @@ async function run() {
     const sliderCollection = client.db('sports-academies').collection('slider')
     const userCollection = client.db('sports-academies').collection('users')
 
+     //jwt
+     app.post('/jwt', (req, res) => {
+       const email = req.body;
+       const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({token})
+    })
+    
+
     //get slider information
     app.get('/slider', async (req, res) => {
       const result = await sliderCollection.find().toArray()
       res.send(result)
     })
-    
-    
-      
+
+   
       //crete user
       app.put('/users/:email', async (req, res) => {
           const email = req.params.email;
@@ -53,6 +78,11 @@ async function run() {
           res.send(result)
           
       })
+    //get al user
+    app.get('/users', verifyJWT, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
